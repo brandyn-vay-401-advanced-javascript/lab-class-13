9ddf1f0bfbc0c51dd46bd93aa33fab635c86b895
KlNGU <brandynvay@yahoo.com>
2019-07-29 19:47:16 -0700

    added files and structure

--- src/auth/router.js
+++ src/auth/router.js
@@ -5,7 +5,6 @@ const authRouter = express.Router();
 
 const User = require('./users-model.js');
 const auth = require('./middleware.js');
-const oauth = require('./oauth/google.js');
 
 authRouter.post('/signup', (req, res, next) => {
   let user = new User(req.body);
@@ -22,14 +21,6 @@ authRouter.post('/signup', (req, res, next) => {
 authRouter.post('/signin', auth, (req, res, next) => {
   res.cookie('auth', req.token);
   res.send(req.token);
-});
-
-authRouter.get('/oauth', (req,res,next) => {
-  oauth.authorize(req)
-    .then( token => {
-      res.status(200).send(token);
-    })
-    .catch(next);
 });
 
 module.exports = authRouter;
